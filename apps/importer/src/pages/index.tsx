17429d33114430bb6bee628b8bad2d1352187d76
Ignacio Ferro Picón <nacho@binpar.com>
2022-11-30 17:39:42 +0100

    feat: text update

--- apps/importer/src/pages/index.tsx
+++ apps/importer/src/pages/index.tsx
@@ -31,7 +31,7 @@ const Home: NextPage = () => {
       <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-8">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">Cognition</span> Importer
+            <span className="text-[hsl(280,100%,70%)]">Cognition Garden Project</span> Data Importer
           </h1>
           <AuthShowcase />
           <div className="flex justify-center px-4 text-2xl">
